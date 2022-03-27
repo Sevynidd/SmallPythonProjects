c6f6d505a8c7427bac5e1074e55750c7db65f868
Sevynidd <Sevynidd02@protonmail.com>
2022-03-27 01:00:06 +0100

    Changed to pygubu

--- CountdownClockAndTimer/MainCountdown.py
+++ CountdownClockAndTimer/MainCountdown.py
@@ -1,44 +1,65 @@
-import tkinter
-from tkinter import *
+import pathlib
+import pygubu
+import tkinter as tk
+import tkinter.ttk as ttk
+
+PROJECT_PATH = pathlib.Path(__file__).parent
+PROJECT_UI = PROJECT_PATH / "main.ui"
 
 
-def get_current_value():
-    return str(int(hours_slider_current_value.get())) + ":"
+class MainApp:
+    def __init__(self, master=None):
+        # build ui
+        self.mainwindow = tk.Tk() if master is None else tk.Toplevel(master)
+        self.Clock = ttk.Label(self.mainwindow)
+        self.Clock.configure(anchor='center', font='{Bahnschrift} 20 {}', justify='right', text='00:00:00')
+        self.Clock.place(anchor='center', x='200', y='40')
+        self.labelHours = ttk.Label(self.mainwindow)
+        self.labelHours.configure(font='{bahnschrift} 10 {}', text='Hours:')
+        self.labelHours.place(anchor='center', x='150', y='80')
+        self.labelMinutes = ttk.Label(self.mainwindow)
+        self.labelMinutes.configure(font='{bahnschrift} 10 {}', text='Minutes:')
+        self.labelMinutes.place(anchor='center', x='150', y='110')
+        self.labelSeconds = ttk.Label(self.mainwindow)
+        self.labelSeconds.configure(font='{bahnschrift} 10 {}', text='Seconds:')
+        self.labelSeconds.place(anchor='center', x='150', y='140')
+        self.buttonStart = ttk.Button(self.mainwindow)
+        self.buttonStart.configure(text='Start')
+        self.buttonStart.place(anchor='center', x='140', y='180')
+        self.buttonStop = ttk.Button(self.mainwindow)
+        self.buttonStop.configure(text='Stop')
+        self.buttonStop.place(anchor='center', x='240', y='180')
+        self.entryHours = ttk.Entry(self.mainwindow)
+        self.entryHours.configure(font='{bahnschrift} 10 {}', width='4')
+        _text_ = '''0'''
+        self.entryHours.delete('0', 'end')
+        self.entryHours.insert('0', _text_)
+        self.entryHours.place(anchor='center', x='230', y='80')
+        self.entryMinutes = ttk.Entry(self.mainwindow)
+        self.entryMinutes.configure(font='{bahnschrift} 10 {}', width='4')
+        _text_ = '''0'''
+        self.entryMinutes.delete('0', 'end')
+        self.entryMinutes.insert('0', _text_)
+        self.entryMinutes.place(anchor='center', x='230', y='110')
+        self.entrySeconds = ttk.Entry(self.mainwindow)
+        self.entrySeconds.configure(font='{bahnschrift} 10 {}', width='4')
+        _text_ = '''0'''
+        self.entrySeconds.delete('0', 'end')
+        self.entrySeconds.insert('0', _text_)
+        self.entrySeconds.place(anchor='center', x='230', y='140')
+        self.mainwindow.configure(height='250', width='400')
+        self.mainwindow.resizable(True, True)
+        self.mainwindow.title('Hello World App')
 
+        # Main widget
+        self.mainwindow = self.mainwindow
 
-def slider_changed(event):
-    hour_label.configure(text=get_current_value())
+    def run(self):
+        self.mainwindow.mainloop()
 
 
 if __name__ == '__main__':
-    root = Tk()
-    root.geometry("300x100")
-    root.title("Counter")
-    root.resizable(False, False)
+    app = MainApp()
+    app.run()
 
-    hours_slider_current_value = tkinter.DoubleVar()
 
-    hour_label = Label(root,
-                       text=get_current_value(),
-                       font=("Courier", 10))
-    hour_label.grid(column=0, row=0)
-
-    minute_label = Label(root,
-                         text="00:",
-                         font=("Courier", 10))
-    minute_label.grid(column=1, row=0)
-
-    second_label = Label(root,
-                         text="00",
-                         font=("Courier", 10))
-    second_label.grid(column=2, row=0)
-
-    hours_slider = Scale(root,
-                         from_=0,
-                         to=60,
-                         orient=HORIZONTAL,
-                         command=slider_changed,
-                         variable=hours_slider_current_value)
-    hours_slider.grid(column=3, row=0)
-
-    root.mainloop()
