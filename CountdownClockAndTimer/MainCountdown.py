5f025c11d1a3d257a25bb63778f3cdb09cc6508c
Sevynidd <sevynidd@web.de>
2022-03-29 12:30:56 +0200

    Clock label

--- CountdownClockAndTimer/MainCountdown.py
+++ CountdownClockAndTimer/MainCountdown.py
@@ -19,10 +19,27 @@ class MainApp:
     def __init__(self, master=None):
         # build ui
         self.mainwindow = tk.Tk() if master is None else tk.Toplevel(master)
-        self.Clock = ttk.Label(self.mainwindow)
-        self.Clock.configure(anchor='center', cursor='arrow', font='{Bahnschrift} 20 {}', justify='right')
-        self.Clock.configure(text='00:00:00')
-        self.Clock.place(anchor='center', x='200', y='40')
+        self.ClockHours = ttk.Label(self.mainwindow)
+        self.ClockHours.configure(anchor='center', cursor='arrow', font='{Bahnschrift} 20 {}', justify='right')
+        self.ClockHours.configure(text='00')
+        self.ClockHours.place(anchor='e', x='170', y='40')
+        self.ClockMinutes = ttk.Label(self.mainwindow)
+        self.ClockMinutes.configure(anchor='center', cursor='arrow', font='{Bahnschrift} 20 {}', justify='right')
+        self.ClockMinutes.configure(text='00')
+        self.ClockMinutes.place(anchor='center', x='200', y='40')
+        self.ClockSeconds = ttk.Label(self.mainwindow)
+        self.ClockSeconds.configure(anchor='center', cursor='arrow', font='{Bahnschrift} 20 {}', justify='right')
+        self.ClockSeconds.configure(text='00')
+        self.ClockSeconds.place(anchor='center', x='240', y='40')
+        self.Doppelpunkt1 = ttk.Label(self.mainwindow)
+        self.Doppelpunkt1.configure(anchor='center', cursor='arrow', font='{Bahnschrift} 20 {}', justify='right')
+        self.Doppelpunkt1.configure(text=':')
+        self.Doppelpunkt1.place(anchor='center', x='180', y='38')
+        self.Doppelpunkt2 = ttk.Label(self.mainwindow)
+        self.Doppelpunkt2.configure(anchor='center', cursor='arrow', font='{Bahnschrift} 20 {}', justify='right')
+        self.Doppelpunkt2.configure(text=':')
+        self.Doppelpunkt2.place(anchor='center', x='220', y='38')
+
         self.labelHours = ttk.Label(self.mainwindow)
         self.labelHours.configure(font='{bahnschrift} 10 {}', text='Hours:')
         self.labelHours.place(anchor='center', x='150', y='80')
@@ -92,9 +109,9 @@ class MainApp:
             if not self.sv_hours.get().isnumeric():
                 self.sv_hours.set(self.sv_hours.get()[:len(self.sv_hours.get()) - 1])
             else:
-                self.Clock["text"] = self.sv_hours.get() + ":" + self.sv_minutes.get() + ":" + self.sv_seconds.get()
+                self.ClockHours["text"] = self.sv_hours.get()
         else:
-            self.Clock["text"] = "0:" + self.sv_minutes.get() + ":" + self.sv_seconds.get()
+            self.ClockHours["text"] = "0"
 
     def entry_callback_minutes(self, var, index, mode):
         if not self.sv_minutes.get() == '':
@@ -103,9 +120,9 @@ class MainApp:
             else:
                 if int(self.sv_minutes.get()) > 60:
                     self.sv_minutes.set(str(60))
-                self.Clock["text"] = self.sv_hours.get() + ":" + self.sv_minutes.get() + ":" + self.sv_seconds.get()
+                self.ClockMinutes["text"] = self.sv_minutes.get()
         else:
-            self.Clock["text"] = self.sv_hours.get() + ":0:" + self.sv_seconds.get()
+            self.ClockMinutes["text"] = "0"
 
     def entry_callback_seconds(self, var, index, mode):
         if not self.sv_seconds.get() == '':
@@ -114,9 +131,9 @@ class MainApp:
             else:
                 if int(self.sv_seconds.get()) > 60:
                     self.sv_seconds.set(str(60))
-                self.Clock["text"] = self.sv_hours.get() + ":" + self.sv_minutes.get() + ":" + self.sv_seconds.get()
+                self.ClockSeconds["text"] = self.sv_seconds.get()
         else:
-            self.Clock["text"] = self.sv_hours.get() + ":" + self.sv_minutes.get() + ":0"
+            self.ClockSeconds["text"] = "0"
 
 
 if __name__ == '__main__':
